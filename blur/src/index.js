761a41d0f87d36872a5c106296c7dcc9f619135d
Christian <chriamue@gmail.com>
2021-03-27 15:56:39 +0100

    :tada: added javascript package for blur

---
+++ blur/src/index.js
@@ -0,0 +1,4 @@
+import "systemjs-webpack-interop/auto-public-path";
+
+const pkg = import('../pkg')
+export const processor = pkg;
---
+++ blur/webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+const systemjsInterop = require("systemjs-webpack-interop/webpack-config");
+
+// Pass in your webpack config, and systemjs-webpack-interop will make it
+// work better with SystemJS
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    mode: 'production',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist/js'),
+        libraryTarget: 'system',
+    },
+    module: {
+        rules: [{ parser: { system: false } },]
+    },
+    experiments: {
+        syncWebAssembly: true
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: false,
+        port: 9000,
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        },
+    },
+};
+
+// Throws errors if your webpack config won't interop well with SystemJS
+systemjsInterop.checkWebpackConfig(module.exports);
