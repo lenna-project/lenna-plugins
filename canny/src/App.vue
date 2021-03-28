fee7578dfff77994c76e2464133fd08bcfcd4dd6
Christian <chriamue@gmail.com>
2021-03-28 11:18:30 +0200

    :sparkles: export ui in canny plugin

--- canny/src/App.vue
+++ canny/src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div v-if="low">
+    <input v-model="low" type="number" />
+    <label>Low</label>
+    <input v-model="high" type="number" />
+    <label>High</label>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Canny",
+  props: {
+    defaultConfig: Object,
   },
-};
+  data() {
+    return {
+      config: {
+        low: 50,
+        high: 100
+      },
+      low: 50,
+      high: 100,
+    };
+  },
+  methods: {
+    async updateConfig(config) {
+      this.$emit("changeConfig", config);
+    },
+  },
+  created() {
+    this.config = this.defaultConfig;
+    this.low = Number(this.config["low"]);
+    this.high = Number(this.config["high"]);
+    this.updateConfig(this.config);
+  },
+});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
--- canny/src/main.js
+++ canny/src/main.js
@@ -1,28 +1,5 @@
-import { h, createApp } from 'vue';
-import singleSpaVue from 'single-spa-vue';
-
 import App from './App.vue';
-const canny = import('canny')
-
-const vueLifecycles = singleSpaVue({
-  createApp,
-  appOptions: {
-    render() {
-      return h(App, {
-        // single-spa props are available on the "this" object. Forward them to your component as needed.
-        // https://single-spa.js.org/docs/building-applications#lifecyle-props
-        // if you uncomment these, remember to add matching prop definitions for them in your App.vue file.
-        /*
-        name: this.name,
-        mountParcel: this.mountParcel,
-        singleSpa: this.singleSpa,
-        */
-      });
-    },
-  },
-});
+const canny = import('canny');
 
 export const processor = canny;
-export const bootstrap = vueLifecycles.bootstrap;
-export const mount = vueLifecycles.mount;
-export const unmount = vueLifecycles.unmount;
+export const ui = App;
