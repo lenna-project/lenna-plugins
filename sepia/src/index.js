69199028aa63d190d50e00c79cc6b6f9e664813f
Christian M <chriamue@gmail.com>
2021-04-15 11:49:56 +0200

    :tada: sepia plugin created

---
+++ sepia/src/index.js
@@ -0,0 +1,12 @@
+import "systemjs-webpack-interop/auto-public-path";
+
+const pkg = import('../pkg')
+export const processor = pkg;
+export const name = () => "sepia";
+export const description = () => "Plugin to sepia image.";
+export const process = async (config, image) => {
+  return import('../pkg').then(processor => processor.process(config, image));
+};
+export const defaultConfig = async () => {
+  return {};
+};
---
+++ sepia/src/lib.rs
@@ -0,0 +1,71 @@
+use image::DynamicImage;
+
+use lenna_core::plugins::PluginRegistrar;
+use lenna_core::Processor;
+use lenna_core::ProcessorConfig;
+use photon_rs::{helpers::dyn_image_from_raw, monochrome::sepia, PhotonImage};
+
+extern "C" fn register(registrar: &mut dyn PluginRegistrar) {
+    registrar.add_plugin(Box::new(Sepia));
+}
+
+#[cfg(feature = "plugin")]
+lenna_core::export_plugin!(register);
+
+#[derive(Default, Clone)]
+pub struct Sepia;
+
+#[derive(Clone, serde::Serialize, serde::Deserialize)]
+struct Config {
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {}
+    }
+}
+
+impl Processor for Sepia {
+    fn name(&self) -> String {
+        "sepia".into()
+    }
+
+    fn title(&self) -> String {
+        "Sepia".into()
+    }
+
+    fn author(&self) -> String {
+        "chriamue".into()
+    }
+
+    fn description(&self) -> String {
+        "Plugin to sepia image.".into()
+    }
+
+    fn process(&self, config: ProcessorConfig, image: DynamicImage) -> DynamicImage {
+        let _config: Config = serde_json::from_value(config.config).unwrap();
+        let mut image: PhotonImage = PhotonImage::new_from_byteslice(image.to_bytes());
+        sepia(&mut image);
+        dyn_image_from_raw(&image)
+    }
+
+    fn default_config(&self) -> serde_json::Value {
+        serde_json::to_value(Config::default()).unwrap()
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+#[cfg(target_arch = "wasm32")]
+lenna_core::export_wasm_plugin!(Sepia);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default() {
+        let sepia = Sepia::default();
+        assert_eq!(sepia.name(), "sepia");
+    }
+}
