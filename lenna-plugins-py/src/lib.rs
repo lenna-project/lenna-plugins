e65e1b3daf741253e031c4656371cbcbf194d3b9
Christian M <chriamue@gmail.com>
2021-04-24 12:32:20 +0200

    added plugins to python module

--- lenna-plugins-py/src/lib.rs
+++ lenna-plugins-py/src/lib.rs
@@ -3,6 +3,11 @@ use pyo3::prelude::*;
 fn lenna_plugins_py(py: Python, m: &PyModule) -> PyResult<()> {
     lenna_core::export_python_plugin!(blur::Blur, "blur", py, m);
     lenna_core::export_python_plugin!(canny::Canny, "canny", py, m);
+    lenna_core::export_python_plugin!(cartoonify::Cartoonify, "cartoonify", py, m);
+    lenna_core::export_python_plugin!(desaturate::Desaturate, "desaturate", py, m);
+    lenna_core::export_python_plugin!(dramatic::Dramatic, "dramatic", py, m);
+    lenna_core::export_python_plugin!(photon_filters::PhotonFilters, "photon_filters", py, m);
     lenna_core::export_python_plugin!(rotate::Rotate, "rotate", py, m);
+    lenna_core::export_python_plugin!(sepia::Sepia, "sepia", py, m);
     Ok(())
 }
