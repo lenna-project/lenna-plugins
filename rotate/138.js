8026134df2b7ca6e4a75884b33d6bdb457e21559
chriamue <chriamue@users.noreply.github.com>
2021-04-25 13:23:55 +0000

    deploy: ca806069ec798ca3b619d3fe5867ef8926fe752e

---
+++ rotate/138.js
@@ -0,0 +1,143 @@
+(self["webpackChunk_lenna_project_rotate"] = self["webpackChunk_lenna_project_rotate"] || []).push([[138,433],{
+
+/***/ 226:
+/***/ (() => {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ 433:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ Widget)
+});
+
+// EXTERNAL MODULE: consume shared module (default) vue@^3.0.11 (strict) (fallback: ./node_modules/@vue/runtime-dom/dist/runtime-dom.esm-bundler.js)
+var runtime_dom_esm_bundler_js_ = __webpack_require__(748);
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[1]!./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/Widget.vue?vue&type=template&id=439dade1&scoped=true
+
+const _withId = /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.withScopeId)("data-v-439dade1")
+
+;(0,runtime_dom_esm_bundler_js_.pushScopeId)("data-v-439dade1")
+const _hoisted_1 = { class: "plugin-config" }
+const _hoisted_2 = { id: "v-model-select-dynamic" }
+;(0,runtime_dom_esm_bundler_js_.popScopeId)()
+
+const render = /*#__PURE__*/_withId((_ctx, _cache, $props, $setup, $data, $options) => {
+  return ((0,runtime_dom_esm_bundler_js_.openBlock)(), (0,runtime_dom_esm_bundler_js_.createBlock)("div", _hoisted_1, [
+    (0,runtime_dom_esm_bundler_js_.createVNode)("div", _hoisted_2, [
+      (0,runtime_dom_esm_bundler_js_.withDirectives)((0,runtime_dom_esm_bundler_js_.createVNode)("select", {
+        "onUpdate:modelValue": _cache[1] || (_cache[1] = $event => (_ctx.selected = $event)),
+        onChange: _cache[2] || (_cache[2] = $event => (_ctx.updateConfig()))
+      }, [
+        ((0,runtime_dom_esm_bundler_js_.openBlock)(true), (0,runtime_dom_esm_bundler_js_.createBlock)(runtime_dom_esm_bundler_js_.Fragment, null, (0,runtime_dom_esm_bundler_js_.renderList)(_ctx.options, (option) => {
+          return ((0,runtime_dom_esm_bundler_js_.openBlock)(), (0,runtime_dom_esm_bundler_js_.createBlock)("option", {
+            value: option.value,
+            key: option.text
+          }, (0,runtime_dom_esm_bundler_js_.toDisplayString)(option.text), 9, ["value"]))
+        }), 128))
+      ], 544), [
+        [runtime_dom_esm_bundler_js_.vModelSelect, _ctx.selected]
+      ])
+    ])
+  ]))
+})
+;// CONCATENATED MODULE: ./src/Widget.vue?vue&type=template&id=439dade1&scoped=true
+
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/Widget.vue?vue&type=script&lang=js
+
+
+/* harmony default export */ const Widgetvue_type_script_lang_js = ((0,runtime_dom_esm_bundler_js_.defineComponent)({
+  name: "RotateWidget",
+  props: {
+    defaultConfig: Object,
+  },
+  data() {
+    return {
+      selected: "90",
+      options: [
+        { text: "90°", value: "90" },
+        { text: "180°", value: "180" },
+        { text: "270°", value: "270" },
+      ],
+    };
+  },
+  methods: {
+    async updateConfig() {
+      let config = {
+        theta: 0.0,
+      };
+      if (this.selected === "90") {
+        config.theta = 90.0;
+      } else if (this.selected === "180") {
+        config.theta = 180.0;
+      } else if (this.selected === "270") {
+        config.theta = 270.0;
+      }
+      this.$emit("changeConfig", config);
+    },
+  },
+  created() {
+    this.updateConfig();
+  },
+}));
+
+;// CONCATENATED MODULE: ./src/Widget.vue?vue&type=script&lang=js
+ 
+// EXTERNAL MODULE: ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-2.use[0]!./node_modules/css-loader/dist/cjs.js!./node_modules/vue-loader/dist/stylePostLoader.js!./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/Widget.vue?vue&type=style&index=0&id=439dade1&scoped=true&lang=css
+var Widgetvue_type_style_index_0_id_439dade1_scoped_true_lang_css = __webpack_require__(226);
+;// CONCATENATED MODULE: ./src/Widget.vue?vue&type=style&index=0&id=439dade1&scoped=true&lang=css
+
+;// CONCATENATED MODULE: ./src/Widget.vue
+
+
+
+
+;
+Widgetvue_type_script_lang_js.render = render
+Widgetvue_type_script_lang_js.__scopeId = "data-v-439dade1"
+
+/* harmony default export */ const Widget = (Widgetvue_type_script_lang_js);
+
+/***/ }),
+
+/***/ 138:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "ui": () => (/* binding */ ui),
+/* harmony export */   "processor": () => (/* binding */ processor),
+/* harmony export */   "name": () => (/* binding */ name),
+/* harmony export */   "description": () => (/* binding */ description),
+/* harmony export */   "process": () => (/* binding */ process),
+/* harmony export */   "defaultConfig": () => (/* binding */ defaultConfig)
+/* harmony export */ });
+/* harmony import */ var _Widget__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(433);
+const pkg = __webpack_require__.e(/* import() */ 844).then(__webpack_require__.bind(__webpack_require__, 844));
+
+
+const ui = _Widget__WEBPACK_IMPORTED_MODULE_0__.default;
+const processor = pkg;
+const name = () => "rotate";
+const description = () => "Plugin to rotate image.";
+const process = async (config, image) => {
+  return __webpack_require__.e(/* import() */ 844).then(__webpack_require__.bind(__webpack_require__, 844)).then(processor => processor.process(config, image));
+};
+const defaultConfig = async () => {
+  return { theta: 90.0 };
+};
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=138.js.map
