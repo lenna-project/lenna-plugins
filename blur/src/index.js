1dc3b36350c51595e7e6f1001dac36d9bf631799
Christian M <chriamue@gmail.com>
2021-06-10 06:46:22 +0200

    :lipstick: added icon to blur

--- blur/src/index.js
+++ blur/src/index.js
@@ -1,6 +1,8 @@
 import "systemjs-webpack-interop/auto-public-path";
 
 const pkg = import('../pkg')
+import blurIcon from '../assets/blur.png';
+
 export const processor = pkg;
 export const name = () => "blur";
 export const description = () => "Plugin to blur image.";
@@ -10,3 +12,6 @@ export const process = async (config, image) => {
 export const defaultConfig = async () => {
   return { sigma: 1.5 };
 };
+export const icon = () => {
+  return blurIcon;
+}
