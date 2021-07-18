1175a06a57a011028e9f1d2f48a729ea32621004
Christian M <chriamue@gmail.com>
2021-07-18 16:05:48 +0200

    :sparkles: :snake: added sharpen plugin to py

--- lenna-plugins-py/src/lib.rs
+++ lenna-plugins-py/src/lib.rs
@@ -9,5 +9,6 @@ fn lenna_plugins_py(py: Python, m: &PyModule) -> PyResult<()> {
     lenna_core::export_python_plugin!(photon_filters::PhotonFilters, "photon_filters", py, m);
     lenna_core::export_python_plugin!(rotate::Rotate, "rotate", py, m);
     lenna_core::export_python_plugin!(sepia::Sepia, "sepia", py, m);
+    lenna_core::export_python_plugin!(sharpen::Sharpen, "sharpen", py, m);
     Ok(())
 }
