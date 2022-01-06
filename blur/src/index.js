92cf1a2b80c43cc50d7e8fb8e077dd3ca0a100a0
Christian M <chriamue@gmail.com>
2022-01-06 17:35:26 +0100

    :recycle::building_construction: change blur plugins webpack

--- blur/src/index.js
+++ blur/src/index.js
@@ -1,17 +1,16 @@
-import "systemjs-webpack-interop/auto-public-path";
-
-const pkg = import('../pkg')
-import blurIcon from '../assets/blur.svg';
+const pkg = import("../pkg");
+import blurIcon from "../assets/blur.svg";
 
 export const processor = pkg;
 export const name = () => "blur";
-export const description = () => "Smoothes image while reducing image noise and reducing detail.";
+export const description = () =>
+  "Smoothes image while reducing image noise and reducing detail.";
 export const process = async (config, image) => {
-  return import('../pkg').then(processor => processor.process(config, image));
+  return import("../pkg").then((processor) => processor.process(config, image));
 };
 export const defaultConfig = async () => {
   return { sigma: 1.5 };
 };
 export const icon = () => {
   return blurIcon;
-}
+};
