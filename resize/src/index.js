c1c177774b393e695fc7d8a34067454b2a272350
Christian M <chriamue@gmail.com>
2022-01-06 17:42:41 +0100

    :recycle::building_construction: change resize plugins webpack

--- resize/src/index.js
+++ resize/src/index.js
@@ -1,17 +1,15 @@
-import 'systemjs-webpack-interop/auto-public-path';
-
-const pkg = import('../pkg')
-import resizeIcon from '../assets/resize.svg';
+const pkg = import("../pkg");
+import resizeIcon from "../assets/resize.svg";
 
 export const processor = pkg;
 export const name = () => "resize";
 export const description = () => "Resizes images.";
 export const process = async (config, image) => {
-  return import('../pkg').then(processor => processor.process(config, image));
+  return import("../pkg").then((processor) => processor.process(config, image));
 };
 export const defaultConfig = async () => {
-  return pkg.then(mod => mod.defaultConfig());
+  return pkg.then((mod) => mod.defaultConfig());
 };
 export const icon = () => {
   return resizeIcon;
-}
+};
--- resize/webpack.config.js
+++ resize/webpack.config.js
@@ -1,17 +1,51 @@
-var path = require("path");
-const systemjsInterop = require("systemjs-webpack-interop/webpack-config");
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const pkg = require("./package.json");
 
-module.exports = {
-  entry: path.resolve(__dirname, "src/index.js"),
+module.exports = (env = {}) => ({
+  name: pkg.config.shortname,
   mode: "production",
+  cache: false,
+  devtool: "source-map",
+  optimization: {
+    minimize: false,
+  },
+  target: "web",
+  entry: path.resolve(__dirname, "./src/index.js"),
   output: {
-    filename: "app.js",
-    path: path.resolve(__dirname, "dist/js"),
-    libraryTarget: "system",
+    publicPath: !env.prod ? "http://localhost:3002/" : pkg.config.publicPath,
+  },
+  resolve: {
+    extensions: [".vue", ".jsx", ".js", ".json"],
+    alias: {
+      vue: "@vue/runtime-dom",
+    },
   },
   module: {
     rules: [
-      { parser: { system: false } },
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.png$/,
+        use: {
+          loader: "url-loader",
+          options: { limit: 8192 },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { hmr: !env.prod },
+          },
+          "css-loader",
+        ],
+      },
       {
         test: /\.svg$/i,
         use: {
@@ -19,30 +53,40 @@ module.exports = {
           options: { limit: 8192 },
         },
       },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 8192,
-            },
-          },
-        ],
-      },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new ModuleFederationPlugin({
+      name: pkg.config.shortname,
+      library: { type: "amd", name: pkg.config.shortname },
+      filename: "remoteEntry.js",
+      remotes: {
+        "lenna-web": "lenna-web",
+      },
+      exposes: {
+        default: "./src/",
+      },
+      remotes: {},
+      shared: ["vue"],
+    }),
+    new VueLoaderPlugin(),
+  ],
   experiments: {
     syncWebAssembly: true,
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    compress: false,
-    port: 8502,
+    contentBase: path.join(__dirname),
+    compress: true,
+    port: 3002,
+    hot: true,
     headers: {
       "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
     },
   },
-};
-
-systemjsInterop.checkWebpackConfig(module.exports);
+});
