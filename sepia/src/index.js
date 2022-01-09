2d09a36edfadf40529d94bbab9f14331ddf0fcbd
Christian M <chriamue@gmail.com>
2022-01-09 08:35:03 +0100

    :recycle: change webpack of sepia plugin

--- sepia/src/index.js
+++ sepia/src/index.js
@@ -1,5 +1,3 @@
-import "systemjs-webpack-interop/auto-public-path";
-
 const pkg = import('../pkg');
 import sepiaIcon from '../assets/sepia.svg';
 
@@ -7,10 +5,10 @@ export const processor = pkg;
 export const name = () => "sepia";
 export const description = () => "Toning images in sepia colors.";
 export const process = async (config, image) => {
-  return import('../pkg').then(processor => processor.process(config, image));
+  return import("../pkg").then((processor) => processor.process(config, image));
 };
 export const defaultConfig = async () => {
-  return {};
+  return pkg.then((mod) => mod.defaultConfig());
 };
 export const icon = () => {
   return sepiaIcon;
--- sepia/webpack.config.js
+++ sepia/webpack.config.js
@@ -1,19 +1,44 @@
-var path = require("path");
-const systemjsInterop = require("systemjs-webpack-interop/webpack-config");
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const { ModuleFederationPlugin } = require("webpack").container;
+const pkg = require("./package.json");
 
-// Pass in your webpack config, and systemjs-webpack-interop will make it
-// work better with SystemJS
-module.exports = {
-  entry: path.resolve(__dirname, "src/index.js"),
+module.exports = (env = {}) => ({
+  name: pkg.config.shortname,
   mode: "production",
+  cache: false,
+  devtool: "source-map",
+  optimization: {
+    minimize: false,
+  },
+  target: "web",
+  entry: path.resolve(__dirname, "./src/index.js"),
   output: {
-    filename: "app.js",
-    path: path.resolve(__dirname, "dist/js"),
-    libraryTarget: "system",
+    publicPath: !env.prod ? "http://localhost:3002/" : pkg.config.publicPath,
+  },
+  resolve: {
+    extensions: [".vue", ".jsx", ".js", ".json"],
+    alias: {
+      vue: "@vue/runtime-dom",
+    },
   },
   module: {
     rules: [
-      { parser: { system: false } },
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.png$/,
+        use: {
+          loader: "url-loader",
+          options: { limit: 8192 },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["css-loader"],
+      },
       {
         test: /\.svg$/i,
         use: {
@@ -21,31 +46,37 @@ module.exports = {
           options: { limit: 8192 },
         },
       },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 8192,
-            },
-          },
-        ],
-      },
     ],
   },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: pkg.config.shortname,
+      library: { type: "amd", name: pkg.config.shortname },
+      filename: "remoteEntry.js",
+      remotes: {
+        "lenna-web": "lenna-web",
+      },
+      exposes: {
+        default: "./src/",
+      },
+      remotes: {},
+      shared: ["vue"],
+    }),
+    new VueLoaderPlugin(),
+  ],
   experiments: {
     syncWebAssembly: true,
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    compress: false,
-    port: 8502,
+    contentBase: path.join(__dirname),
+    compress: true,
+    port: 3002,
+    hot: true,
     headers: {
       "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
     },
   },
-};
-
-// Throws errors if your webpack config won't interop well with SystemJS
-systemjsInterop.checkWebpackConfig(module.exports);
+});
